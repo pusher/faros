20cf579d3e8769ad9d6364b95d96d68181291cc6
Joel Speed <Joel.speed@hotmail.co.uk>
2018-08-15 17:20:43 +0100

    Run all tests with JSON

--- pkg/controller/gittrackobject/gittrackobject_controller_test.go
+++ pkg/controller/gittrackobject/gittrackobject_controller_test.go
@@ -23,6 +23,7 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	farosv1alpha1 "github.com/pusher/faros/pkg/apis/faros/v1alpha1"
+	"github.com/pusher/faros/pkg/utils"
 	"golang.org/x/net/context"
 	appsv1 "k8s.io/api/apps/v1"
 	"k8s.io/api/core/v1"
@@ -84,6 +85,15 @@ var instance *farosv1alpha1.GitTrackObject
 var requests chan reconcile.Request
 var stop chan struct{}
 var stopInformers chan struct{}
+var exampleDeploymentJSON []byte
+var exampleDeploymentJSON2 []byte
+
+func init() {
+	example, _ := utils.YAMLToUnstructured([]byte(exampleDeployment))
+	exampleDeploymentJSON, _ = example.MarshalJSON()
+	example2, _ := utils.YAMLToUnstructured([]byte(exampleDeployment2))
+	exampleDeploymentJSON2, _ = example2.MarshalJSON()
+}
 
 const timeout = time.Second * 5
 
@@ -109,139 +119,177 @@ var _ = Describe("GitTrackObject Suite", func() {
 		close(stopInformers)
 	})
 
-	Describe("When a GitTrackObject resource is created", func() {
-		BeforeEach(func() {
-			instance = &farosv1alpha1.GitTrackObject{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "example",
-					Namespace: "default",
-				},
-				Spec: farosv1alpha1.GitTrackObjectSpec{
-					Name: "deployment-example",
-					Kind: "Deployment",
-					Data: []byte(exampleDeployment),
-				},
-			}
-			// Create the GitTrackObject object and expect the Reconcile to occur
-			err := c.Create(context.TODO(), instance)
-			Expect(err).NotTo(HaveOccurred())
-			Eventually(requests, timeout).Should(Receive(Equal(expectedRequest)))
-		})
+	Describe("When a GitTrackObject resource is created (with YAML)", func() {
+		BeforeEach(func() { CreateInstance([]byte(exampleDeployment)) })
+		AfterEach(DeleteInstance)
 
-		AfterEach(func() {
-			err := c.Delete(context.TODO(), instance)
-			Expect(err).NotTo(HaveOccurred())
-		})
+		It("should create it's child resource", ShouldCreateChild)
 
-		It("should create it's child resource", func() {
-			deploy := &appsv1.Deployment{}
-			Eventually(func() error { return c.Get(context.TODO(), depKey, deploy) }, timeout).
-				Should(Succeed())
+		It("should add an owner reference to the child", ShouldAddOwnerReference)
 
-			// GC not enabled so manually delete the object
-			Expect(c.Delete(context.TODO(), deploy)).To(Succeed())
-		})
-
-		It("should add an owner reference to the child", func() {
-			deploy := &appsv1.Deployment{}
-			Eventually(func() error { return c.Get(context.TODO(), depKey, deploy) }, timeout).
-				Should(Succeed())
-
-			Expect(len(deploy.OwnerReferences)).To(Equal(1))
-			oRef := deploy.OwnerReferences[0]
-			Expect(oRef.APIVersion).To(Equal("faros.pusher.com/v1alpha1"))
-			Expect(oRef.Kind).To(Equal("GitTrackObject"))
-			Expect(oRef.Name).To(Equal(instance.Name))
-
-			// GC not enabled so manually delete the object
-			Expect(c.Delete(context.TODO(), deploy)).To(Succeed())
-		})
-
-		It("should update the GTO status", func() {
-			deploy := &appsv1.Deployment{}
-			Eventually(func() error { return c.Get(context.TODO(), depKey, deploy) }, timeout).
-				Should(Succeed())
-
-			err := c.Get(context.TODO(), depKey, instance)
-			Expect(err).ShouldNot(HaveOccurred())
-			Expect(len(instance.Status.Conditions)).To(Equal(1))
-			condition := instance.Status.Conditions[0]
-			Expect(condition.Type).To(Equal(farosv1alpha1.ObjectInSyncType))
-			Expect(condition.Status).To(Equal(v1.ConditionTrue))
-
-			// GC not enabled so manually delete the object
-			Expect(c.Delete(context.TODO(), deploy)).To(Succeed())
-		})
+		It("should update the GTO status", ShouldUpdateGTOStatus)
 
 		It("should update the resource when the GTO is updated", func() {
-			deploy := &appsv1.Deployment{}
-			Eventually(func() error { return c.Get(context.TODO(), depKey, deploy) }, timeout).
-				Should(Succeed())
-
-			// Fetch the instance and update it
-			Eventually(func() error { return c.Get(context.TODO(), depKey, instance) }, timeout).
-				Should(Succeed())
-			instance.Spec.Data = []byte(exampleDeployment2)
-			err := c.Update(context.TODO(), instance)
-			Expect(err).ShouldNot(HaveOccurred())
-
-			// Wait for a reconcile to happen
-			Eventually(requests, timeout).Should(Receive(Equal(expectedRequest)))
-
-			Eventually(func() error {
-				err := c.Get(context.TODO(), depKey, deploy)
-				if err != nil {
-					return err
-				}
-				if len(deploy.Spec.Template.Spec.Containers) != 1 &&
-					deploy.Spec.Template.Spec.Containers[0].Image != "nginx:latest" {
-					return errors.New("Image not updated")
-				}
-				return nil
-			}, timeout).Should(Succeed())
-			// GC not enabled so manually delete the object
-			Expect(c.Delete(context.TODO(), deploy)).To(Succeed())
+			ShouldUpdateChildOnGTOUpdate([]byte(exampleDeployment2))
 		})
 
-		It("should recreate the child if it is deleted", func() {
-			deploy := &appsv1.Deployment{}
-			Eventually(func() error { return c.Get(context.TODO(), depKey, deploy) }, timeout).
-				Should(Succeed())
+		It("should recreate the child if it is deleted", ShouldRecreateChildIfDeleted)
 
-			// Delete the instance and expect it to be recreated
-			Expect(c.Delete(context.TODO(), deploy)).To(Succeed())
-			Eventually(requests, timeout).Should(Receive(Equal(expectedRequest)))
-			Eventually(func() error { return c.Get(context.TODO(), depKey, deploy) }, timeout).
-				Should(Succeed())
+		It("should reset the child if it is modified", ShouldResetChildIfModified)
+	})
 
-			// GC not enabled so manually delete the object
-			Eventually(func() error { return c.Delete(context.TODO(), deploy) }, timeout).
-				Should(Succeed())
+	Describe("When a GitTrackObject resource is created (with JSON)", func() {
+		BeforeEach(func() { CreateInstance(exampleDeploymentJSON) })
+		AfterEach(DeleteInstance)
+
+		It("should create it's child resource", ShouldCreateChild)
+
+		It("should add an owner reference to the child", ShouldAddOwnerReference)
+
+		It("should update the GTO status", ShouldUpdateGTOStatus)
+
+		It("should update the resource when the GTO is updated", func() {
+			ShouldUpdateChildOnGTOUpdate(exampleDeploymentJSON2)
 		})
 
-		It("should reset the child if it is modified", func() {
-			deploy := &appsv1.Deployment{}
-			Eventually(func() error { return c.Get(context.TODO(), depKey, deploy) }, timeout).
-				Should(Succeed())
+		It("should recreate the child if it is deleted", ShouldRecreateChildIfDeleted)
 
-			// Update the spec and expect it to be reset
-			deploy.Spec.Template.Spec.Containers[0].Image = "nginx:latest"
-			Expect(c.Update(context.TODO(), deploy)).To(Succeed())
-			Eventually(requests, timeout).Should(Receive(Equal(expectedRequest)))
-			Eventually(func() error {
-				err := c.Get(context.TODO(), depKey, deploy)
-				if err != nil {
-					return err
-				}
-				if len(deploy.Spec.Template.Spec.Containers) != 1 &&
-					deploy.Spec.Template.Spec.Containers[0].Image != "nginx" {
-					return errors.New("Image not updated")
-				}
-				return nil
-			}, timeout).Should(Succeed())
-
-			// GC not enabled so manually delete the object
-			Expect(c.Delete(context.TODO(), deploy)).To(Succeed())
-		})
+		It("should reset the child if it is modified", ShouldResetChildIfModified)
 	})
 })
+
+var (
+	CreateInstance = func(data []byte) {
+		instance = &farosv1alpha1.GitTrackObject{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "example",
+				Namespace: "default",
+			},
+			Spec: farosv1alpha1.GitTrackObjectSpec{
+				Name: "deployment-example",
+				Kind: "Deployment",
+				Data: data,
+			},
+		}
+		// Create the GitTrackObject object and expect the Reconcile to occur
+		err := c.Create(context.TODO(), instance)
+		Expect(err).NotTo(HaveOccurred())
+		Eventually(requests, timeout).Should(Receive(Equal(expectedRequest)))
+	}
+
+	DeleteInstance = func() {
+		err := c.Delete(context.TODO(), instance)
+		Expect(err).NotTo(HaveOccurred())
+	}
+
+	ShouldCreateChild = func() {
+		deploy := &appsv1.Deployment{}
+		Eventually(func() error { return c.Get(context.TODO(), depKey, deploy) }, timeout).
+			Should(Succeed())
+
+		// GC not enabled so manually delete the object
+		Expect(c.Delete(context.TODO(), deploy)).To(Succeed())
+	}
+
+	ShouldAddOwnerReference = func() {
+		deploy := &appsv1.Deployment{}
+		Eventually(func() error { return c.Get(context.TODO(), depKey, deploy) }, timeout).
+			Should(Succeed())
+
+		Expect(len(deploy.OwnerReferences)).To(Equal(1))
+		oRef := deploy.OwnerReferences[0]
+		Expect(oRef.APIVersion).To(Equal("faros.pusher.com/v1alpha1"))
+		Expect(oRef.Kind).To(Equal("GitTrackObject"))
+		Expect(oRef.Name).To(Equal(instance.Name))
+
+		// GC not enabled so manually delete the object
+		Expect(c.Delete(context.TODO(), deploy)).To(Succeed())
+	}
+
+	ShouldUpdateGTOStatus = func() {
+		deploy := &appsv1.Deployment{}
+		Eventually(func() error { return c.Get(context.TODO(), depKey, deploy) }, timeout).
+			Should(Succeed())
+
+		err := c.Get(context.TODO(), depKey, instance)
+		Expect(err).ShouldNot(HaveOccurred())
+		Expect(len(instance.Status.Conditions)).To(Equal(1))
+		condition := instance.Status.Conditions[0]
+		Expect(condition.Type).To(Equal(farosv1alpha1.ObjectInSyncType))
+		Expect(condition.Status).To(Equal(v1.ConditionTrue))
+
+		// GC not enabled so manually delete the object
+		Expect(c.Delete(context.TODO(), deploy)).To(Succeed())
+	}
+
+	ShouldUpdateChildOnGTOUpdate = func(data []byte) {
+		deploy := &appsv1.Deployment{}
+		Eventually(func() error { return c.Get(context.TODO(), depKey, deploy) }, timeout).
+			Should(Succeed())
+
+		// Fetch the instance and update it
+		Eventually(func() error { return c.Get(context.TODO(), depKey, instance) }, timeout).
+			Should(Succeed())
+		instance.Spec.Data = data
+		err := c.Update(context.TODO(), instance)
+		Expect(err).ShouldNot(HaveOccurred())
+
+		// Wait for a reconcile to happen
+		Eventually(requests, timeout).Should(Receive(Equal(expectedRequest)))
+
+		Eventually(func() error {
+			err := c.Get(context.TODO(), depKey, deploy)
+			if err != nil {
+				return err
+			}
+			if len(deploy.Spec.Template.Spec.Containers) != 1 &&
+				deploy.Spec.Template.Spec.Containers[0].Image != "nginx:latest" {
+				return errors.New("Image not updated")
+			}
+			return nil
+		}, timeout).Should(Succeed())
+		// GC not enabled so manually delete the object
+		Expect(c.Delete(context.TODO(), deploy)).To(Succeed())
+	}
+
+	ShouldRecreateChildIfDeleted = func() {
+		deploy := &appsv1.Deployment{}
+		Eventually(func() error { return c.Get(context.TODO(), depKey, deploy) }, timeout).
+			Should(Succeed())
+
+		// Delete the instance and expect it to be recreated
+		Expect(c.Delete(context.TODO(), deploy)).To(Succeed())
+		Eventually(requests, timeout).Should(Receive(Equal(expectedRequest)))
+		Eventually(func() error { return c.Get(context.TODO(), depKey, deploy) }, timeout).
+			Should(Succeed())
+
+		// GC not enabled so manually delete the object
+		Eventually(func() error { return c.Delete(context.TODO(), deploy) }, timeout).
+			Should(Succeed())
+	}
+
+	ShouldResetChildIfModified = func() {
+		deploy := &appsv1.Deployment{}
+		Eventually(func() error { return c.Get(context.TODO(), depKey, deploy) }, timeout).
+			Should(Succeed())
+
+		// Update the spec and expect it to be reset
+		deploy.Spec.Template.Spec.Containers[0].Image = "nginx:latest"
+		Expect(c.Update(context.TODO(), deploy)).To(Succeed())
+		Eventually(requests, timeout).Should(Receive(Equal(expectedRequest)))
+		Eventually(func() error {
+			err := c.Get(context.TODO(), depKey, deploy)
+			if err != nil {
+				return err
+			}
+			if len(deploy.Spec.Template.Spec.Containers) != 1 &&
+				deploy.Spec.Template.Spec.Containers[0].Image != "nginx" {
+				return errors.New("Image not updated")
+			}
+			return nil
+		}, timeout).Should(Succeed())
+
+		// GC not enabled so manually delete the object
+		Expect(c.Delete(context.TODO(), deploy)).To(Succeed())
+	}
+)
