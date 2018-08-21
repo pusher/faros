b2763efcb51af20709ff3f9c0ec0ee5d184fa23f
Joel Speed <Joel.speed@hotmail.co.uk>
2018-08-21 14:18:06 +0100

    Move GetAPIResource to utils

--- pkg/utils/rest_mapper.go
+++ pkg/utils/rest_mapper.go
@@ -20,6 +20,7 @@ import (
 	"fmt"
 
 	"k8s.io/apimachinery/pkg/api/meta"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/discovery"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/restmapper"
@@ -38,4 +39,15 @@ func NewRestMapper(config *rest.Config) (meta.RESTMapper, error) {
 	}
 
 	return restmapper.NewDiscoveryRESTMapper(apiGroupResources), nil
+}
+
+// GetAPIResource uses a rest mapper to get the GroupVersionResource and
+// determine whether an object is namespaced or not
+func GetAPIResource(restMapper meta.RESTMapper, gvk schema.GroupVersionKind) (schema.GroupVersionResource, bool, error) {
+	fqKind := schema.FromAPIVersionAndKind(gvk.ToAPIVersionAndKind())
+	mapping, err := restMapper.RESTMapping(fqKind.GroupKind(), fqKind.Version)
+	if err != nil {
+		return schema.GroupVersionResource{}, false, fmt.Errorf("unable to get REST mapping: %v", err)
+	}
+	return mapping.Resource, mapping.Scope == meta.RESTScopeNamespace, nil
 }
