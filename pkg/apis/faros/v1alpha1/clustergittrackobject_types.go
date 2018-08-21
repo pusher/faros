5a2231e590acbe418c204913d97e04d9f664f7a4
Joel Speed <Joel.speed@hotmail.co.uk>
2018-08-21 11:17:49 +0100

    Kubebuilder create ClusterGitTrackObject

---
+++ pkg/apis/faros/v1alpha1/clustergittrackobject_types.go
@@ -0,0 +1,64 @@
+/*
+Copyright 2018 Pusher Ltd.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// ClusterGitTrackObjectSpec defines the desired state of ClusterGitTrackObject
+type ClusterGitTrackObjectSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+// ClusterGitTrackObjectStatus defines the observed state of ClusterGitTrackObject
+type ClusterGitTrackObjectStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +genclient:nonNamespaced
+
+// ClusterGitTrackObject is the Schema for the clustergittrackobjects API
+// +k8s:openapi-gen=true
+type ClusterGitTrackObject struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ClusterGitTrackObjectSpec   `json:"spec,omitempty"`
+	Status ClusterGitTrackObjectStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +genclient:nonNamespaced
+
+// ClusterGitTrackObjectList contains a list of ClusterGitTrackObject
+type ClusterGitTrackObjectList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ClusterGitTrackObject `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&ClusterGitTrackObject{}, &ClusterGitTrackObjectList{})
+}
