35cb46f056c932cc08e1fe0d906de8f2b7ed8bf5
Joel Speed <Joel.speed@hotmail.co.uk>
2018-10-11 15:45:47 +0100

    Disable metrics while testing

--- pkg/controller/gittrack/gittrack_controller_suite_test.go
+++ pkg/controller/gittrack/gittrack_controller_suite_test.go
@@ -108,6 +108,7 @@ func SetupTestReconcile(inner reconcile.Reconciler) (reconcile.Reconciler, chan 
 func StartTestManager(mgr manager.Manager) chan struct{} {
 	stop := make(chan struct{})
 	go func() {
+		defer GinkgoRecover()
 		Expect(mgr.Start(stop)).NotTo(HaveOccurred())
 	}()
 	return stop
