880aa57c822bfde7fe0a5e1546eed86c7094e844
Joel Speed <Joel.speed@hotmail.co.uk>
2019-08-28 12:36:48 +0100

    Copy over handler code to make tests pass

--- pkg/controller/gittrack/handler.go
+++ pkg/controller/gittrack/handler.go
@@ -17,10 +17,14 @@ limitations under the License.
 package gittrack
 
 import (
+	"fmt"
+	"strings"
 	"time"
 
 	farosv1alpha1 "github.com/pusher/faros/pkg/apis/faros/v1alpha1"
 	gittrackutils "github.com/pusher/faros/pkg/controller/gittrack/utils"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
 // handlerResult is the single return object from handleGitTrack
@@ -44,5 +48,87 @@ type handlerResult struct {
 }
 
 func (r *ReconcileGitTrack) handleGitTrack(gt farosv1alpha1.GitTrackInterface) handlerResult {
-	return handlerResult{}
+	var result handlerResult
+
+	// Get a map of the files that are in the Spec
+	files, err := r.getFiles(gt)
+	if err != nil {
+		result.gitError = err
+		result.gitReason = gittrackutils.ErrorFetchingFiles
+		return result
+	}
+	// Git successful, set condition
+	result.gitReason = gittrackutils.GitFetchSuccess
+	r.recorder.Eventf(gt, corev1.EventTypeNormal, "CheckoutSuccessful", "Successfully checked out '%s' at '%s'", gt.GetSpec().Repository, gt.GetSpec().Reference)
+
+	// Attempt to parse k8s objects from files
+	objects, fileErrors := objectsFrom(files)
+	result.ignoredFiles = fileErrors
+	result.ignored += int64(len(fileErrors))
+	if len(fileErrors) > 0 {
+		var errs []string
+		for file, reason := range fileErrors {
+			errs = append(errs, fmt.Sprintf("%s: %s", file, reason))
+		}
+		result.parseError = fmt.Errorf(strings.Join(errs, ",\n"))
+		result.parseReason = gittrackutils.ErrorParsingFiles
+	} else {
+		result.parseReason = gittrackutils.FileParseSuccess
+	}
+
+	// Update status with the number of objects discovered
+	result.discovered = int64(len(objects))
+
+	// Get a list of the GitTrackObjects that currently exist, by name
+	objectsByName, err := r.listObjectsByName(gt)
+	if err != nil {
+		return result
+	}
+	// Process the objects and feed back the results
+	resultsChan := make(chan objectResult, len(objects))
+	for _, obj := range objects {
+		go func(obj *unstructured.Unstructured) {
+			resultsChan <- r.handleObject(obj, gt)
+		}(obj)
+	}
+
+	handlerErrors := []string{}
+	// Iterate through results and update status accordingly
+	for range objects {
+		res := <-resultsChan
+		if res.Ignored {
+			result.ignoredFiles[res.NamespacedName] = res.Reason
+			result.ignored++
+		} else {
+			result.applied++
+		}
+		result.timeToDeploy = append(result.timeToDeploy, res.TimeToDeploy)
+		if res.InSync {
+			result.inSync++
+		}
+		delete(objectsByName, res.NamespacedName)
+		if res.Error != nil {
+			handlerErrors = append(handlerErrors, res.Error.Error())
+		}
+	}
+
+	// If there were errors updating the child objects, set the ChildrenUpToDate
+	// condition appropriately
+	if len(handlerErrors) > 0 {
+		result.upToDateError = fmt.Errorf(strings.Join(handlerErrors, ",\n"))
+		result.upToDateReason = gittrackutils.ErrorUpdatingChildren
+	} else {
+		result.upToDateReason = gittrackutils.ChildrenUpdateSuccess
+	}
+
+	// Cleanup potentially leftover resources
+	if err = r.deleteResources(objectsByName); err != nil {
+		result.gcError = err
+		result.gcReason = gittrackutils.ErrorDeletingChildren
+		r.recorder.Eventf(gt, corev1.EventTypeWarning, "CleanupFailed", "Failed to clean-up leftover resources")
+		return result
+	}
+	result.gcReason = gittrackutils.GCSuccess
+
+	return result
 }
