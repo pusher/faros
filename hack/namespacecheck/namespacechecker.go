f99ef214142d72e5195d6e653ddc47cc7251a419
Daniel Morsing <daniel.morsing@pusher.com>
2019-11-13 14:18:04 +0000

    remove comment from namespacecheck, it is now in the readme

--- hack/namespacecheck/namespacechecker.go
+++ hack/namespacecheck/namespacechecker.go
@@ -14,21 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// namespacecheck checks whether a given Faros setup
-// is valid within the ownership rules added in v0.7.
-//
-// To run the tool, grab 3 files off your kubernetes cluster
-//
-// # kubectl get gittracks --all-namespaces -o json > gt.json
-// # kubectl get gittrackobjects --all-namespaces -o json > gto.json
-// # kubectl get clustergittrackobjects -all-namespaces -o json > cgto.json
-//
-// Then run
-// ./namespacecheck --gt-file gt.json --gto-file gto.json --cgto-file cgto.json
-//
-// The tool will report back if you have any cross namespace ownership
-// and write a file containing any new clustergittracks that will need to
-// be created
 package main
 
 import (
